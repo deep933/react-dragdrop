a574fa42b6d398b5a58c3f29cf1459dbd92dd199
deepmal933 <deepmal944@gmail.com>
2019-12-10 00:02:51 -0500

    added webpack & babel to transpile the code

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    filename: "index.js"
+  },
+  mode:'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
